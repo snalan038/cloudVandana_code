610937ffa2c687020f5b92163594bda177b92bbb
Nalan S <snalan038@gmail.com>
2023-10-23 18:27:15 +0530

    Add files via upload

---
+++ Javascript/question_1.js
@@ -0,0 +1,10 @@
+
+let a="This is a Sunny day"
+
+let b=""
+
+for(let i=a.length-1;i>=0;i--){
+    b=b+a.charAt(i)
+}
+
+console.log(b)
---
+++ Javascript/question_2.js
@@ -0,0 +1,4 @@
+let a=[5,8,3,4,9,2,2,1]
+a.sort((a,b)=>b-a)
+
+console.log(a)
